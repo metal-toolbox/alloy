3338b8cb51eb2c47bc0219b52ba2889483165d8c
Joel Rebello <jrebello@packet.com>
2022-07-15 14:40:43 +0200

    internal/collect: define interface, errors

---
+++ internal/collect/interface.go
@@ -0,0 +1,18 @@
+package collect
+
+import (
+	"context"
+)
+
+const (
+	KindInBand    = "inband"
+	KindOutOfBand = "outOfBand"
+)
+
+// Collector interface defines methods to collect device inventory
+type Collector interface {
+	// Inventory listens on the AssetCh collects inventory for all assets until the AssetCh is closed.
+	Inventory(ctx context.Context) error
+	// SetMockGetter sets a mock device inventory getter to be used for tests.
+	SetMockGetter(getter interface{})
+}
