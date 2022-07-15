ae68e720e1757060d144ccc6ed5f77a62bb5777f
Joel Rebello <jrebello@packet.com>
2022-07-15 14:32:38 +0200

    app: App holds attributes to initialize Alloy

---
+++ internal/app/app.go
@@ -0,0 +1,122 @@
+package app
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/metal-toolbox/alloy/internal/model"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+const (
+	KindInband    = "inband"
+	KindOutOfBand = "outofband"
+	// PublishTarget default, collected inventory data is published to hollow server service.
+	PublishTarget = "hollow"
+)
+
+var (
+	ErrAppInit = errors.New("error initializing app")
+)
+
+// App holds attributes for running alloy.
+type App struct {
+	// App configuration.
+	Config *model.Config
+	// AssetCh is where the asset getter retrieves assets from the asset store for the inventory collector to consume.
+	AssetCh chan *model.Asset
+	// CollectorCh is where the asset inventory information is written, for the publisher to consume.
+	CollectorCh chan *model.AssetDevice
+	// TermCh is the channel to terminate the app based on a signal
+	TermCh chan os.Signal
+	// Sync waitgroup to wait for running go routines on termination.
+	SyncWg *sync.WaitGroup
+	// Logger is the app logger
+	Logger *logrus.Logger
+}
+
+// New returns a new alloy application object with the configuration loaded
+func New(ctx context.Context, kind, cfgFile string, loglevel int) (app *App, err error) {
+	switch kind {
+	case KindInband:
+	case KindOutOfBand:
+	default:
+		return nil, errors.Wrap(ErrAppInit, "invalid app kind: "+kind)
+	}
+
+	cfg, err := configLoad(kind, cfgFile)
+	if err != nil {
+		return nil, errors.Wrap(ErrAppInit, "config load error: "+err.Error())
+	}
+
+	cfg.AppKind = kind
+
+	app = &App{
+		Config:      cfg,
+		AssetCh:     make(chan *model.Asset),
+		CollectorCh: make(chan *model.AssetDevice),
+		TermCh:      make(chan os.Signal),
+		SyncWg:      &sync.WaitGroup{},
+		Logger:      logrus.New(),
+	}
+
+	switch loglevel {
+	case model.LogLevelDebug:
+		app.Logger.Level = logrus.DebugLevel
+	case model.LogLevelTrace:
+		app.Logger.Level = logrus.TraceLevel
+	default:
+		app.Logger.Level = logrus.InfoLevel
+	}
+
+	app.Logger.SetFormatter(&logrus.JSONFormatter{})
+
+	// register for SIGINT, SIGTERM
+	signal.Notify(app.TermCh, syscall.SIGINT, syscall.SIGTERM)
+
+	return app, nil
+}
+
+func configLoad(kind, cfgFile string) (config *model.Config, err error) {
+	cfg := configDefault()
+
+	if cfgFile == "" {
+		return cfg, nil
+	}
+
+	cfg.File = cfgFile
+	viper.SetConfigFile(cfgFile)
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	err = viper.Unmarshal(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+// configDefault returns a default app configuration
+func configDefault() *model.Config {
+	return &model.Config{
+		AppKind:  KindInband,
+		LogLevel: 0,
+	}
+}
+
+func NewLogrusEntryFromLogger(fields logrus.Fields, logger *logrus.Logger) *logrus.Entry {
+	l := logrus.New()
+	l.Formatter = logger.Formatter
+	l.Level = logger.Level
+
+	return logger.WithFields(fields)
+}
