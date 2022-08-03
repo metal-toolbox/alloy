240623808b10e45eb6c6100802f222af667d474a
Joel Rebello <jrebello@packet.com>
2022-08-03 14:32:45 +0200

    asset/serverservice: implement Getter interface for serverService

---
+++ internal/asset/serverservice_test.go
@@ -0,0 +1,320 @@
+package asset
+
+import (
+	"context"
+	"net"
+
+	// _ "net/http/pprof"
+	"os"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/gammazero/workerpool"
+	"github.com/metal-toolbox/alloy/internal/app"
+	"github.com/metal-toolbox/alloy/internal/fixtures"
+	"github.com/metal-toolbox/alloy/internal/model"
+	"github.com/stretchr/testify/assert"
+	serverservice "go.hollow.sh/serverservice/pkg/api/v1"
+)
+
+func newMockServerServiceGetter(t *testing.T, alloy *app.App) *serverServiceGetter {
+	t.Helper()
+
+	return &serverServiceGetter{
+		logger:  alloy.Logger.WithField("component", "test"),
+		syncWg:  alloy.SyncWg,
+		config:  alloy.Config,
+		assetCh: alloy.AssetCh,
+		workers: workerpool.New(1),
+		client:  fixtures.NewMockServerServiceClient(),
+	}
+}
+
+func Test_ServerServiceListByIDs(t *testing.T) {
+	// left commented out here to indicate a means of debugging
+	//	go func() {
+	//		log.Println(http.ListenAndServe("localhost:9091", nil))
+	// }()
+	// init alloy app
+	alloy, err := app.New(context.TODO(), app.KindOutOfBand, "", model.LogLevelTrace)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := []*model.Asset{}
+	expected := []*model.Asset{
+		fixtures.MockAssets["foo"],
+		fixtures.MockAssets["bar"],
+	}
+
+	var collectTimedOut bool
+
+	// background routine to receive asset objects objects sent from the asset getter
+	// mocks a collector
+	alloy.SyncWg.Add(1)
+
+	go func(t *testing.T, wg *sync.WaitGroup) {
+		t.Helper()
+
+		defer wg.Done()
+
+		timeout := time.NewTicker(time.Second * 5).C
+	Loop:
+		for {
+			select {
+			case asset, ok := <-alloy.AssetCh:
+				if !ok {
+					break Loop
+				}
+				got = append(got, asset)
+			case <-timeout:
+				collectTimedOut = true
+				break Loop
+			}
+		}
+	}(t, alloy.SyncWg)
+
+	// init asset getter
+	getter := newMockServerServiceGetter(t, alloy)
+
+	err = getter.ListByIDs(context.TODO(), []string{"foo", "bar"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// wait for routines to complete
+	alloy.SyncWg.Wait()
+
+	// test inventory items match expected
+	assert.ElementsMatch(t, expected, got)
+	assert.False(t, collectTimedOut)
+}
+
+func Test_ServerServiceListAll(t *testing.T) {
+	// nolint:govet // test struct is clearer to read in this alignment
+	testcases := []struct {
+		batchSize   int
+		totalAssets int
+		name        string
+	}{
+		{
+			1,
+			1,
+			"total == batch size",
+		},
+		{
+			1,
+			11,
+			"total > batch size",
+		},
+		{
+			20,
+			11,
+			"total < batch size",
+		},
+	}
+
+	os.Setenv("TEST_ENV", "1")
+	defer os.Unsetenv("TEST_ENV")
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			var collectTimedOut bool
+			got := []*model.Asset{}
+
+			assetCh := make(chan *model.Asset)
+
+			alloy, err := app.New(context.Background(), app.KindOutOfBand, "", model.LogLevelInfo)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			alloy.Config.ServerService.Concurrency = model.ConcurrencyDefault
+
+			getter := newMockServerServiceGetter(t, alloy)
+			getter.assetCh = assetCh
+
+			// rigs the mock AssetsByOffsetLimit to return the total count of assets
+			os.Setenv("FIXTURE_TOTAL_ASSETS", strconv.Itoa(tc.totalAssets))
+			defer os.Unsetenv("FIXTURE_TOTAL_ASSETS")
+
+			// background routine to receive asset objects objects sent from the asset getter
+			// mocks a collector
+			getter.syncWg.Add(1)
+			go func(t *testing.T) {
+				t.Helper()
+				defer getter.syncWg.Done()
+
+				timeout := time.NewTicker(time.Second*delayBetweenRequests + 5).C
+			Loop:
+				for {
+					select {
+					case asset, ok := <-assetCh:
+						if !ok {
+							break Loop
+						}
+						got = append(got, asset)
+					case <-timeout:
+						collectTimedOut = true
+						break Loop
+					}
+				}
+			}(t)
+
+			err = getter.ListAll(context.TODO())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// wait for routines to return
+			getter.syncWg.Wait()
+
+			assert.Equal(t, tc.totalAssets, len(got))
+			assert.False(t, collectTimedOut)
+		})
+	}
+}
+
+func Test_validateRequiredAttribtues(t *testing.T) {
+	cases := []struct {
+		name        string
+		server      *serverservice.Server
+		secret      *serverservice.ServerSecret
+		expectedErr string
+	}{
+		{
+			"server object nil",
+			nil,
+			nil,
+			"server object nil",
+		},
+		{
+			"server secret object nil",
+			&serverservice.Server{},
+			nil,
+			"server secret object nil",
+		},
+		{
+			"server attributes slice empty",
+			&serverservice.Server{},
+			&serverservice.ServerSecret{},
+			"server attributes slice empty",
+		},
+		{
+			"server secret value empty",
+			&serverservice.Server{Attributes: []serverservice.Attributes{{Namespace: attributeNamespace}}},
+			&serverservice.ServerSecret{},
+			"BMC secret empty",
+		},
+		{
+			"server secret value invalid",
+			&serverservice.Server{Attributes: []serverservice.Attributes{{Namespace: attributeNamespace}}},
+			&serverservice.ServerSecret{Value: "foo"},
+			"invalid BMC secret",
+		},
+		{
+			"server secret value invalid",
+			&serverservice.Server{Attributes: []serverservice.Attributes{{Namespace: attributeNamespace}}},
+			&serverservice.ServerSecret{Value: "foo:"},
+			"invalid BMC secret",
+		},
+		{
+			"server secret value invalid",
+			&serverservice.Server{Attributes: []serverservice.Attributes{{Namespace: attributeNamespace}}},
+			&serverservice.ServerSecret{Value: ":asdasd"},
+			"invalid BMC secret",
+		},
+		{
+			"objects with valid attributes",
+			&serverservice.Server{Attributes: []serverservice.Attributes{{Namespace: attributeNamespace}}},
+			&serverservice.ServerSecret{Value: "user:hunter2"},
+			"",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := validateRequiredAttributes(tc.server, tc.secret)
+			if tc.expectedErr != "" {
+				assert.Contains(t, err.Error(), tc.expectedErr)
+				return
+			}
+
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func Test_toAsset(t *testing.T) {
+	cases := []struct {
+		name          string
+		server        *serverservice.Server
+		secret        *serverservice.ServerSecret
+		expectedAsset *model.Asset
+		expectedErr   string
+	}{
+		{
+			"Expected attributes empty raises error",
+			&serverservice.Server{
+				Attributes: []serverservice.Attributes{
+					{
+						Namespace: "invalid",
+					},
+				},
+			},
+			&serverservice.ServerSecret{Value: "user:hunter2"},
+			nil,
+			"expected server attributes, got none",
+		},
+		{
+			"Attributes missing BMC IP Address raises error",
+			&serverservice.Server{
+				Attributes: []serverservice.Attributes{
+					{
+						Namespace: attributeNamespace,
+						Data:      []byte(`{"foo": "bar"}`),
+					},
+				},
+			},
+			&serverservice.ServerSecret{Value: "user:hunter2"},
+			nil,
+			"expected attribute empty: BMCIPAddress",
+		},
+		{
+			"Valid server, secret objects returns *model.Asset object",
+			&serverservice.Server{
+				Attributes: []serverservice.Attributes{
+					{
+						Namespace: attributeNamespace,
+						Data:      []byte(`{"BMCIPAddress": "127.0.0.1", "Vendor": "foo", "Model": "bar"}`),
+					},
+				},
+			},
+			&serverservice.ServerSecret{Value: "user:hunter2"},
+			&model.Asset{
+				ID:          "00000000-0000-0000-0000-000000000000",
+				Vendor:      "foo",
+				Model:       "bar",
+				Facility:    "",
+				BMCUsername: "user",
+				BMCPassword: "hunter2",
+				BMCAddress:  net.ParseIP("127.0.0.1"),
+			},
+			"",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			asset, err := toAsset(tc.server, tc.secret)
+			if tc.expectedErr != "" {
+				assert.Contains(t, err.Error(), tc.expectedErr)
+				return
+			}
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expectedAsset, asset)
+		})
+	}
+}
