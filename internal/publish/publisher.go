226c33450335684472418f4147f95f8af29086aa
Joel Rebello <jrebello@packet.com>
2022-08-11 10:32:53 +0200

    publisher: Add PublishOne() method to publish assets directly
    This method is for publishing asset without invoking the publisher
    collector channel iterator.

--- internal/publish/publisher.go
+++ internal/publish/publisher.go
@@ -2,10 +2,15 @@ package publish
 
 import (
 	"context"
+
+	"github.com/metal-toolbox/alloy/internal/model"
 )
 
 // Publisher defines an interface for device inventory to be published to remote endpoints.
 type Publisher interface {
 	// Run spawns a device inventory publisher
 	Run(ctx context.Context) error
+
+	// PublishOne publishes the given device information to the configured publish target
+	PublishOne(ctx context.Context, device *model.AssetDevice) error
 }
