8894f2ac8cf212a547059d7197d2441676411f3b
Joel Rebello <jrebello@packet.com>
2022-08-11 14:13:19 +0200

    collect: rename interface method Inventory to InventoryRemote
    This also defines and implements the InventoryLocal interface method

--- internal/collect/interface.go
+++ internal/collect/interface.go
@@ -2,12 +2,20 @@ package collect
 
 import (
 	"context"
+
+	"github.com/metal-toolbox/alloy/internal/model"
 )
 
 // Collector interface defines methods to collect device inventory
 type Collector interface {
-	// Inventory listens on the AssetCh collects inventory for all assets until the AssetCh is closed.
-	Inventory(ctx context.Context) error
+	// InventoryRemote spawns an iterator that listens on the AssetCh and
+	// collects inventory remotely (through its BMC) for each of the assets until the AssetCh is closed,
+	// the collected inventory is then sent on the collector channel to be published.
+	InventoryRemote(ctx context.Context) error
+
+	// InventoryLocal collects and returns inventory on the local host for the given asset.
+	InventoryLocal(ctx context.Context) (*model.AssetDevice, error)
+
 	// SetMockGetter sets a mock device inventory getter to be used for tests.
 	SetMockGetter(getter interface{})
 }
