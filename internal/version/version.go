042c20e48a532e50ca8ae54c4c57110a7488dac5
Joel Rebello <jrebello@packet.com>
2022-11-22 11:21:38 +0100

    cmd/version: Adds Alloy version command
    ❯ ./alloy version
    commit: 86269a3
    branch: alloy-inband
    git summary: v0.1.0-9-g86269a3-dirty
    buildDate: 1669110157
    Go version: go1.19
    ironlib version: v0.2.1
    bmclib version: v2.0.1-0.20221111104230-8b19316c39fd

---
+++ internal/version/version.go
@@ -0,0 +1,87 @@
+package version
+
+import (
+	"runtime"
+	rdebug "runtime/debug"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	GitCommit      string
+	GitBranch      string
+	GitSummary     string
+	BuildDate      string
+	AppVersion     string
+	IronlibVersion = ironlibVersion()
+	BmclibVersion  = bmclibVersion()
+	GoVersion      = runtime.Version()
+)
+
+type Version struct {
+	GitCommit      string `json:"git_commit"`
+	GitBranch      string `json:"git_branch"`
+	GitSummary     string `json:"git_summary"`
+	BuildDate      string `json:"build_date"`
+	AppVersion     string `json:"app_version"`
+	GoVersion      string `json:"go_version"`
+	IronlibVersion string `json:"ironlib_version"`
+	BmclibVersion  string `json:"bmclib_version"`
+}
+
+func Current() Version {
+	return Version{
+		GitBranch:      GitBranch,
+		GitCommit:      GitCommit,
+		GitSummary:     GitSummary,
+		BuildDate:      BuildDate,
+		AppVersion:     AppVersion,
+		GoVersion:      GoVersion,
+		IronlibVersion: IronlibVersion,
+		BmclibVersion:  BmclibVersion,
+	}
+}
+
+func ExportBuildInfoMetric() {
+	buildInfo := promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "alloy_build_info",
+			Help: "A metric with a constant '1' value, labeled by branch, commit, summary, builddate, version, goversion from which fup was built.",
+		},
+		[]string{"branch", "commit", "summary", "builddate", "version", "goversion", "ironlibversion"},
+	)
+
+	buildInfo.WithLabelValues(GitBranch, GitCommit, GitSummary, BuildDate, AppVersion, GoVersion, IronlibVersion).Set(1)
+}
+
+func ironlibVersion() string {
+	buildInfo, ok := rdebug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+
+	for _, d := range buildInfo.Deps {
+		if strings.Contains(d.Path, "ironlib") {
+			return d.Version
+		}
+	}
+
+	return ""
+}
+
+func bmclibVersion() string {
+	buildInfo, ok := rdebug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+
+	for _, d := range buildInfo.Deps {
+		if strings.Contains(d.Path, "bmclib") {
+			return d.Version
+		}
+	}
+
+	return ""
+}
