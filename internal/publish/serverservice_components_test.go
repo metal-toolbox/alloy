0ea87e3f9649e1a3f9e0c6138999c1355daba529
Diogo Mendes Matsubara <diogo.mendesmatsubara@eu.equinix.com>
2023-02-02 19:25:05 +0100

    Fix tests

--- internal/publish/serverservice_components_test.go
+++ internal/publish/serverservice_components_test.go
@@ -1,6 +1,8 @@
 package publish
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/google/uuid"
@@ -12,12 +14,29 @@ import (
 )
 
 func Test_ToComponentSlice(t *testing.T) {
-	h := serverServicePublisher{
-		logger:               logrus.NewEntry(logrus.New()),
-		slugs:                fixtures.ServerServiceSlugMap(),
-		attributeNS:          model.ServerComponentAttributeNS(model.AppKindOutOfBand),
-		versionedAttributeNS: model.ServerComponentVersionedAttributeNS(model.AppKindOutOfBand),
-	}
+	handler := http.NewServeMux()
+
+	// get firmwares query
+	handler.HandleFunc(
+		"/api/v1/server-component-firmwares",
+		func(w http.ResponseWriter, r *http.Request) {
+			switch r.Method {
+			case http.MethodGet:
+				w.Header().Set("Content-Type", "application/json")
+
+				_, _ = w.Write([]byte(`{}`))
+			default:
+				t.Fatal("expected GET request, got: " + r.Method)
+			}
+		},
+	)
+
+	mock := httptest.NewServer(handler)
+	p := testPublisherInstance(t, mock.URL)
+	p.logger = logrus.NewEntry(logrus.New())
+	p.slugs = fixtures.ServerServiceSlugMap()
+	p.attributeNS = model.ServerComponentAttributeNS(model.AppKindOutOfBand)
+	p.versionedAttributeNS = model.ServerComponentVersionedAttributeNS(model.AppKindOutOfBand)
 
 	testcases := []struct {
 		name     string
@@ -26,24 +45,24 @@ func Test_ToComponentSlice(t *testing.T) {
 	}{
 		{
 			"E3C246D4INL",
-			&model.Asset{Inventory: fixtures.CopyDevice(fixtures.E3C246D4INL)},
+			&model.Asset{Vendor: "asrockrack", Inventory: fixtures.CopyDevice(fixtures.E3C246D4INL)},
 			componentPtrSlice(fixtures.ServerServiceE3C246D4INLcomponents),
 		},
 		{
 			"R6515_A",
-			&model.Asset{Inventory: fixtures.CopyDevice(fixtures.R6515_f0c8e4ac)},
+			&model.Asset{Vendor: "dell", Inventory: fixtures.CopyDevice(fixtures.R6515_f0c8e4ac)},
 			componentPtrSlice(fixtures.ServerServiceR6515Components_f0c8e4ac),
 		},
 		{
 			"R6515_B",
-			&model.Asset{Inventory: fixtures.CopyDevice(fixtures.R6515_fc167440)},
+			&model.Asset{Vendor: "dell", Inventory: fixtures.CopyDevice(fixtures.R6515_fc167440)},
 			componentPtrSlice(fixtures.ServerServiceR6515Components_fc167440),
 		},
 	}
 
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-			sc, err := h.toComponentSlice(uuid.Nil, tc.device)
+			sc, err := p.toComponentSlice(uuid.Nil, tc.device)
 			if err != nil {
 				t.Fatal(err)
 			}
