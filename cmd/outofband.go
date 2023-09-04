6ba5ff1c16b7001a85d448a1766739221a4f6ebb
Joel Rebello <jrebello@packet.com>
2023-09-04 18:18:51 +0200

    cmd/outofband: --worker flag runs Alloy as a worker listening for Conditions on NATS JS

--- cmd/outofband.go
+++ cmd/outofband.go
@@ -6,10 +6,10 @@ import (
 
 	"github.com/metal-toolbox/alloy/internal/app"
 	"github.com/metal-toolbox/alloy/internal/collector"
-	"github.com/metal-toolbox/alloy/internal/controller"
 	"github.com/metal-toolbox/alloy/internal/helpers"
 	"github.com/metal-toolbox/alloy/internal/metrics"
 	"github.com/metal-toolbox/alloy/internal/model"
+	"github.com/metal-toolbox/alloy/internal/worker"
 	"github.com/spf13/cobra"
 	"go.hollow.sh/toolbox/events"
 	"golang.org/x/net/context"
@@ -30,9 +30,11 @@ var (
 	// csvfile holds the path to the csv file
 	csvFile string
 
-	// controllerMode when enabled runs alloy in controller mode to periodically fetch data for all assets in the
-	// configured store.
-	controllerMode bool
+	// facilityCode to limit Alloy to when running as a worker.
+	facilityCode string
+
+	// asWorker when true runs Alloy as a worker listening on the NATS JS for Conditions to reconcile.
+	asWorker bool
 )
 
 // outofband inventory, bios configuration collection command
@@ -64,15 +66,13 @@ var cmdOutofband = &cobra.Command{
 			cancelFunc()
 		}()
 
-		if len(assetIDs) > 0 {
-			runOnAssets(ctx, alloy)
-
+		switch {
+		case asWorker:
+			runWorker(ctx, alloy)
 			return
-		}
 
-		if controllerMode {
-			runController(ctx, alloy)
-
+		case len(assetIDs) > 0:
+			runOnAssets(ctx, alloy)
 			return
 		}
 
@@ -80,18 +80,18 @@ var cmdOutofband = &cobra.Command{
 	},
 }
 
-func runController(ctx context.Context, alloy *app.App) {
-	streamBroker, err := events.NewStreamBroker(*alloy.Config.NatsOptions)
+func runWorker(ctx context.Context, alloy *app.App) {
+	stream, err := events.NewStream(*alloy.Config.NatsOptions)
 	if err != nil {
 		alloy.Logger.Fatal(err)
 	}
 
-	acontroller, err := controller.New(ctx, streamBroker, alloy.Config, alloy.SyncWg, alloy.Logger)
+	w, err := worker.New(ctx, facilityCode, stream, alloy.Config, alloy.SyncWg, alloy.Logger)
 	if err != nil {
 		alloy.Logger.Fatal(err)
 	}
 
-	acontroller.Run(ctx)
+	w.Run(ctx)
 }
 
 func runOnAssets(ctx context.Context, alloy *app.App) {
@@ -121,7 +121,8 @@ func init() {
 	cmdOutofband.PersistentFlags().DurationVar(&splay, "collect-splay", app.DefaultCollectSplay, "splay adds jitter to the collection interval")
 	cmdOutofband.PersistentFlags().StringSliceVar(&assetIDs, "asset-ids", []string{}, "Collect inventory for the given comma separated list of asset IDs.")
 	cmdOutofband.PersistentFlags().StringVar(&csvFile, "csv-file", "assets.csv", "CSV file containing BMC credentials for assets.")
-	cmdOutofband.PersistentFlags().BoolVarP(&controllerMode, "controller-mode", "", false, "Run Alloy in a controller loop that periodically refreshes inventory, BIOS configuration data in the store and listens for events.")
+	cmdOutofband.PersistentFlags().StringVar(&facilityCode, "facility-code", "sandbox", "The facility code this Alloy instance is associated with")
+	cmdOutofband.PersistentFlags().BoolVar(&asWorker, "worker", false, "Run Alloy as a worker listening for conditions on NATS")
 
 	rootCmd.AddCommand(cmdOutofband)
 }
