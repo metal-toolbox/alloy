9b803fecf01274ccbca1640f8e6709bffbd7374c
Alva Zhang <140438385+Alva8756@users.noreply.github.com>
2024-03-20 10:25:17 -0700

    Create types for sending to CIS (#194)
    * Create types for sending to CIS
    
    * rename for less long

---
+++ types/types.go
@@ -0,0 +1,12 @@
+package types
+
+import (
+	"github.com/bmc-toolbox/common"
+)
+
+type BiosConfig map[string]string
+
+type InventoryDevice struct {
+	Inv     *common.Device `json:"inventory,omitempty"`
+	BiosCfg *BiosConfig    `json:"biosconfig,omitempty"`
+}
