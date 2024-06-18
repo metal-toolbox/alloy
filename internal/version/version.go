cedc1be063e79829cc9bc9428002caefc31f8bb8
Joel Rebello <jrebello@packet.com>
2024-06-18 10:40:38 +0200

    cmd/version: rename hss references to fleetdb

--- internal/version/version.go
+++ internal/version/version.go
@@ -10,40 +10,40 @@ import (
 )
 
 var (
-	GitCommit            string
-	GitBranch            string
-	GitSummary           string
-	BuildDate            string
-	AppVersion           string
-	IronlibVersion       = ironlibVersion()
-	BmclibVersion        = bmclibVersion()
-	ServerserviceVersion = serverserviceVersion()
-	GoVersion            = runtime.Version()
+	GitCommit         string
+	GitBranch         string
+	GitSummary        string
+	BuildDate         string
+	AppVersion        string
+	IronlibVersion    = ironlibVersion()
+	BmclibVersion     = bmclibVersion()
+	FleetDBAPIVersion = fleetdbAPIVersion()
+	GoVersion         = runtime.Version()
 )
 
 type Version struct {
-	GitCommit            string `json:"git_commit"`
-	GitBranch            string `json:"git_branch"`
-	GitSummary           string `json:"git_summary"`
-	BuildDate            string `json:"build_date"`
-	AppVersion           string `json:"app_version"`
-	GoVersion            string `json:"go_version"`
-	IronlibVersion       string `json:"ironlib_version"`
-	BmclibVersion        string `json:"bmclib_version"`
-	ServerserviceVersion string `json:"serverservice_version"`
+	GitCommit         string `json:"git_commit"`
+	GitBranch         string `json:"git_branch"`
+	GitSummary        string `json:"git_summary"`
+	BuildDate         string `json:"build_date"`
+	AppVersion        string `json:"app_version"`
+	GoVersion         string `json:"go_version"`
+	IronlibVersion    string `json:"ironlib_version"`
+	BmclibVersion     string `json:"bmclib_version"`
+	FleetDBAPIVersion string `json:"fleetdbapi_version"`
 }
 
 func Current() Version {
 	return Version{
-		GitBranch:            GitBranch,
-		GitCommit:            GitCommit,
-		GitSummary:           GitSummary,
-		BuildDate:            BuildDate,
-		AppVersion:           AppVersion,
-		GoVersion:            GoVersion,
-		IronlibVersion:       IronlibVersion,
-		BmclibVersion:        BmclibVersion,
-		ServerserviceVersion: ServerserviceVersion,
+		GitBranch:         GitBranch,
+		GitCommit:         GitCommit,
+		GitSummary:        GitSummary,
+		BuildDate:         BuildDate,
+		AppVersion:        AppVersion,
+		GoVersion:         GoVersion,
+		IronlibVersion:    IronlibVersion,
+		BmclibVersion:     BmclibVersion,
+		FleetDBAPIVersion: FleetDBAPIVersion,
 	}
 }
 
@@ -53,10 +53,10 @@ func ExportBuildInfoMetric() {
 			Name: "alloy_build_info",
 			Help: "A metric with a constant '1' value, labeled by branch, commit, summary, builddate, version, goversion from which Alloy was built.",
 		},
-		[]string{"branch", "commit", "summary", "builddate", "version", "goversion", "ironlibversion", "serverserviceVersion"},
+		[]string{"branch", "commit", "summary", "builddate", "version", "goversion", "ironlibversion", "fleetdbAPIVersion"},
 	)
 
-	buildInfo.WithLabelValues(GitBranch, GitCommit, GitSummary, BuildDate, AppVersion, GoVersion, IronlibVersion, ServerserviceVersion).Set(1)
+	buildInfo.WithLabelValues(GitBranch, GitCommit, GitSummary, BuildDate, AppVersion, GoVersion, IronlibVersion, FleetDBAPIVersion).Set(1)
 }
 
 func ironlibVersion() string {
@@ -89,14 +89,14 @@ func bmclibVersion() string {
 	return ""
 }
 
-func serverserviceVersion() string {
+func fleetdbAPIVersion() string {
 	buildInfo, ok := rdebug.ReadBuildInfo()
 	if !ok {
 		return ""
 	}
 
 	for _, d := range buildInfo.Deps {
-		if strings.Contains(d.Path, "serverservice") {
+		if strings.Contains(d.Path, "fleetdb") {
 			return d.Version
 		}
 	}
